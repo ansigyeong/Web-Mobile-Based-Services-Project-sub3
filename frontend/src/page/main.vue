cba292c0c957744def3a4d9fc22000e27695af90
안시경 <jeongseok95@naver.com>
2020-07-28 02:19:00 +0900

    S03P12D108-11

---
+++ frontend/src/page/main.vue
@@ -0,0 +1,18 @@
+><template>
+  <div>
+    <img src="./assets/img/logo.jpg" alt="main image">
+    <Search @input-change="onInputChage"/>
+  </div>
+</template>
+
+<script>
+import Search from '../src/components/common/Search.vue'
+
+export default {
+    name: 'main'
+}
+</script>
+
+<style>
+
+</style>
