9efd0bb7e309b3c5a8b11fa5bcc12a4fa0e2475a
안시경 <jeongseok95@naver.com>
2020-07-28 14:56:49 +0900

    S03P12D108-12

--- frontend/src/page/post/AskQuestion.vue
+++ frontend/src/page/post/AskQuestion.vue
@@ -15,9 +15,6 @@
         <v-row>
           <v-col cols="12" sm="6">
             <v-select
-              v-model="form.favoriteAnimal"
-              :items="animals"
-              :rules="rules.animal"
               color="pink"
               label="언어 선택"
               required
@@ -25,22 +22,11 @@
           </v-col>
           <v-col cols="12" sm="6">
             <v-text-field
-              v-model="form.first"
-              :rules="rules.name"
               color="purple darken-2"
               label="제목을 입력하세요."
               required
             ></v-text-field>
           </v-col>
-          <!-- <v-col cols="12" sm="6">
-            <v-text-field
-              v-model="form.last"
-              :rules="rules.name"
-              color="blue darken-2"
-              label=""
-              required
-            ></v-text-field>
-          </v-col> -->
           <v-col cols="12">
             <v-textarea
               v-model="form.bio"
@@ -49,38 +35,10 @@
               <template v-slot:label>
                 <div>
                   내용을 입력하세요.
-                  <!-- 내용을 입력하세요. <small>(optional)</small> -->
                 </div>
               </template>
             </v-textarea>
           </v-col>
-          <!-- <v-col cols="12" sm="6">
-            <v-slider
-              v-model="form.age"
-              :rules="rules.age"
-              color="orange"
-              label="Age"
-              hint="Be honest"
-              min="1"
-              max="100"
-              thumb-label
-            ></v-slider>
-          </v-col> -->
-          <!-- <v-col cols="12">
-            <v-checkbox
-              v-model="form.terms"
-              color="green"
-            >
-              <template v-slot:label>
-                <div @click.stop="">
-                  Do you accept the
-                  <a href="javascript:;" @click.stop="terms = true">terms</a>
-                  and
-                  <a href="javascript:;" @click.stop="conditions = true">conditions?</a>
-                </div>
-              </template>
-            </v-checkbox>
-          </v-col> -->
         </v-row>
       </v-container>
       <v-card-actions>
@@ -134,41 +92,16 @@
     name: "AskQuestion",
     data () {
       const defaultForm = Object.freeze({
-        first: '',
-        last: '',
-        bio: '',
-        favoriteAnimal: '',
-        age: null,
         terms: false,
       })
 
       return {
         form: Object.assign({}, defaultForm),
-        rules: {
-          age: [
-            val => val < 10 || `I don't believe you!`,
-          ],
-          animal: [val => (val || '').length > 0 || 'This field is required'],
-          name: [val => (val || '').length > 0 || 'This field is required'],
-        },
-        animals: ['Dog', 'Cat', 'Rabbit', 'Turtle', 'Snake'],
         conditions: false,
         content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc.`,
         snackbar: false,
-        terms: false,
         defaultForm,
       }
-    },
-
-    computed: {
-      formIsValid () {
-        return (
-          this.form.first &&
-          this.form.last &&
-          this.form.favoriteAnimal &&
-          this.form.terms
-        )
-      },
     },
 
     methods: {
