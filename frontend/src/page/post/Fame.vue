bdf84ad19be909437e1e7a70aed77cde7cf7974e
안시경 <jeongseok95@naver.com>
2020-07-29 11:50:51 +0900

    S03P12D108-28

--- frontend/src/page/post/Fame.vue
+++ frontend/src/page/post/Fame.vue
@@ -1,13 +1,127 @@
 <template>
-  
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="434"
+      tile
+    >
+      <v-img
+        height="100%"
+        src="@/assets/img/바다.jpg"
+      >
+        <v-row
+          align="end"
+          class="fill-height"
+        >
+          <v-col
+            align-self="start"
+            class="pa-0"
+            cols="12"
+          >
+            <v-avatar
+              class="profile"
+              color="grey"
+              size="164"
+              tile
+            >
+              <v-img src="@/assets/img/백상아리.png"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col class="py-0">
+            <v-list-item
+              color="rgba(0, 0, 0, .4)"
+              dark
+            >
+              <v-list-item-content>
+                <v-list-item-title class="title">홍길동</v-list-item-title>
+                <v-list-item-subtitle>백상아리</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-card>
+    <v-card
+      class="mx-auto"
+      max-width="434"
+      tile
+    >
+      <v-img
+        height="100%"
+        src="@/assets/img/바다.jpg"
+      >
+        <v-row
+          align="end"
+          class="fill-height"
+        >
+          <v-col
+            align-self="start"
+            class="pa-0"
+            cols="12"
+          >
+            <v-avatar
+              class="profile"
+              color="grey"
+              size="164"
+              tile
+            >
+              <v-img src="@/assets/img/돌고래.png"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col class="py-0">
+            <v-list-item
+              color="rgba(0, 0, 0, .4\)"
+              dark
+            >
+              <v-list-item-content>
+                <v-list-item-title class="title">김길동</v-list-item-title>
+                <v-list-item-subtitle>돌고래</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-card>
+    <v-card
+      class="mx-auto"
+      max-width="434"
+      tile
+    >
+      <v-img
+        height="100%"
+        src="@/assets/img/바다.jpg"
+      >
+        <v-row
+          align="end"
+          class="fill-height"
+        >
+          <v-col
+            align-self="start"
+            class="pa-0"
+            cols="12"
+          >
+            <v-avatar
+              class="profile"
+              color="grey"
+              size="164"
+              tile
+            >
+              <v-img src="@/assets/img/돌고래.png"></v-img>
+            </v-avatar>
+          </v-col>
+          <v-col class="py-0">
+            <v-list-item
+              color="rgba(0, 0, 0, .4)"
+              dark
+            >
+              <v-list-item-content>
+                <v-list-item-title class="title">최길동</v-list-item-title>
+                <v-list-item-subtitle>돌고래</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-card>
+  </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
