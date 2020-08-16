4d9d0e8267556dae6f7b524179155e1b2b8e0e9d
안시경 <jeongseok95@naver.com>
2020-08-16 21:18:49 +0900

    S03P13D108-2

--- frontend/src/page/post/main.vue
+++ frontend/src/page/post/main.vue
@@ -1,69 +1,23 @@
 <template>
-<div>
-     <full-page :options="options" id="fullpage">
+  <div>
+    <full-page :options="options" id="fullpage">
 
-<!-- 1 -->
-  <div style="" class="section" >
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-      
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-    </b-carousel>
-  </div>
-<!-- 2 -->
-<div class="section">
-    <div id="carousel">
-      <carousel-3d  style="height: 550px;">
-          <span v-for="(item, idx) in items" :key="idx">
-              <slide :index="idx">
-            
-                <div class="crs-bx" style="border: solid 7px #ffd700;">
-                  <img class="grade-img" :src="item.recodeImg"/>
-                  <div class="text-box">
-                    <h2>{{item.level}}</h2>
-                    <h3>{{item.animal}}</h3>
-                  </div>
-                </div>
-          </span>
-        
+      <!-- 1 -->
+      <div style="" class="section" >
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="4000"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1024"
+          img-height="480"
+          style="text-shadow: 1px 1px 2px #333;"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+          
+        >
           <!-- Text slides with image -->
           <b-carousel-slide
             caption="First slide"
@@ -92,7 +46,7 @@
               >
             </template>
           </b-carousel-slide>
-        </carousel-3d>
+        </b-carousel>
       </div>
 
       <!-- 2 -->
